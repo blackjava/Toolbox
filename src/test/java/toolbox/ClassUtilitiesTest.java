194a92921262e70b7534d309efcfa70577248027
Espen Gronli <espen@znork.net>
2014-10-25 23:03:58 +0200

    Added default Spring context for wiring.

--- src/test/java/toolbox/ClassUtilitiesTest.java
+++ src/test/java/toolbox/ClassUtilitiesTest.java
@@ -8,21 +8,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import toolbox.classutilitiestest.NewClass;
 
 public class ClassUtilitiesTest {
+    private ApplicationContext context = new ClassPathXmlApplicationContext("defaultSpringApplicationContext.xml");
+    private ClassUtilities classUtilities = context.getBean(ClassUtilities.class);
 
     @Test
     public void get_classes_in_non_existing_package() throws Exception {
-        ApplicationContext context = new ClassPathXmlApplicationContext("springApplicationContext.xml");
-        ClassUtilities classUtilities = context.getBean(ClassUtilities.class);
-        
         List<Class> classes = classUtilities.getClassesInPackage("toolbox.classutilitiestest.nonexistentpackage");
         assertTrue(classes.isEmpty());
     }
     
     @Test
     public void get_classes_in_package() throws Exception {
-        ApplicationContext context = new ClassPathXmlApplicationContext("springApplicationContext.xml");
-        ClassUtilities classUtilities = context.getBean(ClassUtilities.class);
-        
         List<Class> classes = classUtilities.getClassesInPackage("toolbox.classutilitiestest");
         assertEquals(1, classes.size());
         assertEquals(NewClass.class, classes.get(0));
